fe970a341747e37b9e71476281f7a3b9a2181a67
Ravi Suri <rsuriprog@gmail.com>
2019-01-19 21:15:03 -0500

    Add files via upload
    Updated files to add jointables, result conversion to csv, other improvements.

--- sqlcmdr.go
+++ sqlcmdr.go
@@ -5,8 +5,13 @@ import (
 	"strconv"
 	"reflect"
 	_ "github.com/mattn/go-sqlite3"
+   	"encoding/csv"
+   	"os"
+   	"strings"
    	"fmt"
 )
+
+/////////////////////////// structs ////////////////////////////////////
 
 type InsertCmd struct{
 	Tablename string
@@ -26,33 +31,46 @@ type SelectCmd struct{
 
 type JoinTable struct{
 	Type string
-	LTablename string
-	RTablename string
-	Leftcol string
-	Rightcol string
+	LSide string  			//e.g. "LeftTable-Column"
+	RSide string 			//e.g. "RightTable-Column"
+	
 }
 
-func InitDB() *sql.DB {
-	database, _ := sql.Open("sqlite3", "./data.db")
-	return database
-}
-func JustRunIt( command string ){
-	conn := InitDB()
-	statement, _ := conn.Prepare( command )			   
-	statement.Exec()
-	conn.Close();
-}
+////////////////////////// public funcs ////////////////////////////////
 
-func (icmd *InsertCmd) Add(column string, value interface{} ){
+
+func (icmd *InsertCmd) Add (column string, value interface{} ){
 	icmd.columns = append( icmd.columns, column )
 	icmd.values = append( icmd.values, value )
 }
 
+func (scmd *SelectCmd) AddJoin ( leftside string, rightside string, jointype string){
+	jt := JoinTable{ Type: jointype, LSide: leftside, RSide: rightside }
+	scmd.Joins = append( scmd.Joins, jt )
+}
+
+func InitDB() *sql.DB {
+	// opens connection, needs to be closed with conn.Close later
+	database, _ := sql.Open("sqlite3", "./data.db")
+	return database
+}
+func JustRunIt( command string ){
+	// just runs command with own connection.  No return, no parameterization
+	conn := InitDB()
+	defer conn.Close();
+	statement, err := conn.Prepare( command )		
+	checkErr( err, "JUST RUN" )	   
+	statement.Exec()
+}
+
 func Insert( conn *sql.DB, icmd InsertCmd){
+	// runs insert based on InsertCmd
+	
+	// convert icmd into string insert command
 	colstring := ""
 	valstring := ""
 	for _, col :=  range icmd.columns 	{
-		if( len(colstring) > 0 )		{
+		if( len(colstring) > 0 )	{
 			colstring = colstring + ", "
 			valstring = valstring + ", "
 		}
@@ -60,6 +78,8 @@ func Insert( conn *sql.DB, icmd InsertCmd){
 		valstring = valstring + "?"
 	}
 	sqlcmd := "INSERT INTO " + icmd.Tablename + " (" + colstring + " ) VALUES (" + valstring + ")"
+	
+	// prepare and run
 	statement, err := conn.Prepare(sqlcmd)
 	checkErr( err, "SELECT PREPARE" )
 	
@@ -69,8 +89,9 @@ func Insert( conn *sql.DB, icmd InsertCmd){
 }
 
 func Select( conn *sql.DB, scmd SelectCmd ) [][]interface{}{
-	// returns array of array 
+	// runs select based on SelectCmd, returns array of array that can then be processed with resultstring/resultcsv
 	
+	// covert scmd object into string for query
 	cols := ""
 	if( scmd.RowID ){
 		cols = cols+"rowid, "
@@ -81,7 +102,8 @@ func Select( conn *sql.DB, scmd SelectCmd ) [][]interface{}{
 	if( !selectall ){
 		sqlcmd += " WHERE " + scmd.Keycol + scmd.Comparison + " ? "
 	}
-	
+	fmt.Println( sqlcmd )
+	// do the query
 	var rows *sql.Rows
 	var err error
 	if( !selectall ){
@@ -89,7 +111,7 @@ func Select( conn *sql.DB, scmd SelectCmd ) [][]interface{}{
 	}else{
 		rows, err = conn.Query( sqlcmd )
 	}
-	fmt.Println( sqlcmd )
+	
 	checkErr( err, "SELECT" )
     
     // build an array of interface pointers because this is needed by scan    
@@ -116,29 +138,58 @@ func Select( conn *sql.DB, scmd SelectCmd ) [][]interface{}{
 	return retvals
 }
 
-func ResultString( retvals [][]interface{} ) string{
+func ResultCSV( records [][]interface{}, output string ) string{
+	// turns 2d array of interfaces into output csv file
+	file, err := os.Create(output)
+	checkErr( err, "CSV to " + output )
+	w := csv.NewWriter(file)
+	defer file.Close()
+	for _, record := range records {
+		var strrecord []string
+		for _, record2 := range record {
+			strrecord = append( strrecord, cell2string( record2 ) )			
+		}
+		err := w.Write(strrecord)
+		checkErr( err, "error writing record to csv:" )
+	}
+
+	w.Flush()  
+	if err := w.Error(); err != nil {
+		panic(err)
+	}
+	return "output to " + output
+}
+
+func ResultString( records [][]interface{} ) string{
+	// turns 2d array of intefaces into single string
 	var result string
-	for _, element := range( retvals ){
+	for _, element := range( records ){
 		for _, e2 := range( element ){
-			if( e2 != nil ){
-				switch e2.(type){
-					case []uint8 : 
-						result = result + " " + string(e2.([]uint8))
-					case float64 : 
-						result = result + " " + strconv.FormatFloat(e2.(float64), 'f', 6, 64)
-					case int64 :
-						result = result + " " + strconv.FormatInt( e2.(int64), 10 )
-					default :
-						result = result + " " + "ERR: unhandled type "+ reflect.TypeOf(e2).String()
-					}
-			}
+			result += cell2string( e2 );
 		}
 		result = result + "\n"
 	}
 	return result
 }
 
-/////////////////// privates ////////////////////////////////////
+/////////////////// private funcs ////////////////////////////////////
+
+func cell2string( e2 interface{} ) string{
+	result := "";
+	if( e2 != nil ){
+			switch e2.(type){
+				case []uint8 : 
+					result =  " " + string(e2.([]uint8))
+				case float64 : 
+					result =  " " + strconv.FormatFloat(e2.(float64), 'f', 6, 64)
+				case int64 :
+					result =  " " + strconv.FormatInt( e2.(int64), 10 )
+				default :
+					result =  " " + "ERR: unhandled type "+ reflect.TypeOf(e2).String()
+				}
+		}
+	return result
+}
 
 func checkErr(err error, origin string) {
 	if err != nil {
@@ -149,9 +200,11 @@ func checkErr(err error, origin string) {
 func joinstr( joins []JoinTable ) string{
 	str := ""
 	for _, join := range joins{
-		str += join.Type + " JOIN " + join.RTablename + 
-				" ON " + join.LTablename + "." + join.Leftcol + 
-				" = " + join.RTablename + "." + join.Rightcol + " "
+		lsides := strings.Split( join.LSide, "-" );
+		rsides := strings.Split( join.RSide, "-" );
+		str += join.Type + " JOIN " + rsides[0]+ 
+				" ON " + lsides[0] + "." + lsides[1] + 
+				" = " + rsides[0] + "." + rsides[1] + " "
 	}
 	return str
 }
